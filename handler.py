9909a4cbb0a5833b025f14a3aa20d4276e41aeb2
Sophia Castellarin <scastellarin@anaconda.com>
2019-03-12 16:12:00 -0500

    Deploy docs to gh-pages

--- handler.py
+++ handler.py
@@ -3,25 +3,24 @@ import os, shutil
 import base64, requests
 from sphinx.application import Sphinx
 from boto3 import client as boto3_client
-from serverless_secrets import secrets
 
 
 lambda_client = boto3_client('lambda', region_name="us-east-1",)
-ssm_client = boto3.client('ssm')
+ssm_client = boto3_client('ssm')
 
 
 def get_secret(key):
-	resp = ssm_client.get_parameter(Name=key, WithDecryption=True)
-	return resp['Parameter']['Value']
+    resp = ssm_client.get_parameter(Name=key, WithDecryption=True)
+    return resp['Parameter']['Value']
 
 
-def validate_signature(headers, event_body, github_token):
+def validate_signature(headers, event_body, github_webhook_token):
     try:
         signature = headers['X-Hub-Signature']
         _, sha1 = signature.split('=')
     except:
         return "Bad Request"
-    digest = hmac.new(github_token.encode(), event_body.encode(), hashlib.sha1).hexdigest()
+    digest = hmac.new(github_webhook_token.encode(), event_body.encode(), hashlib.sha1).hexdigest()
     if not hmac.compare_digest(digest.encode(), sha1.encode()):
         return "Not Authorized"
 
@@ -39,13 +38,20 @@ def build(event, context):
     SPHINXBUILD = os.getenv('SPHINXBUILD', 'sphinx-build')
     json_payload = json.loads(event["body"])
     github_repo = json_payload["repository"]['html_url']
+    trimed_github_repo = github_repo.split("//")[1]
     pr_number = json_payload["number"]
     repo_path = "/tmp/conda"
     try:
         shutil.rmtree(repo_path)
     except FileNotFoundError:
         pass
-    git.exec_command("clone", github_repo, repo_path)
+
+    conda_bot_token = get_secret("conda_bot_token")
+    github_user = os.getenv("github_user")
+    github_email = os.getenv("github_email")
+    authed_repo = "https://%s:%s@%s" % (github_user, conda_bot_token, trimed_github_repo)
+
+    git.exec_command("clone", authed_repo, repo_path)
     os.chdir(repo_path)
     git.exec_command("fetch", "origin", "+refs/pull/%s/head:pr/%s" % (pr_number, pr_number), cwd=repo_path)
     git.exec_command("checkout", "pr/%s" % pr_number, cwd=repo_path)
@@ -61,12 +67,13 @@ def build(event, context):
 
     git.exec_command("checkout", "gh-pages", cwd=repo_path)
     git.exec_command("add", "pr-%s" % pr_number, cwd=repo_path)
-
-    # commit_env['GIT_AUTHOR_NAME'] = 'conda-bot'
-    # commit_env['GIT_AUTHOR_EMAIL'] = 'distro-team@anaconda.com'
-    # commit_env['GIT_SSH_COMMAND'] = "ssh -i /tmp/deploy-key"
-    #
-    # git.exec_command("push", "origin", "gh-pages", cwd=repo_path, env=commit_env)
+    commit_env = os.environ
+    commit_env['GIT_AUTHOR_NAME'] = github_user
+    commit_env['GIT_AUTHOR_EMAIL'] = github_email
+    commit_env['GIT_COMMITTER_NAME'] = github_user
+    commit_env['GIT_COMMITTER_EMAIL'] = github_email
+    git.exec_command("commit", "-m docs for pr %s" % pr_number, cwd=repo_path, env=commit_env)
+    git.exec_command("push", "origin", "gh-pages", cwd=repo_path)
 
     response_body = "built docs change"
 
@@ -79,7 +86,7 @@ def build(event, context):
 
 
 def build_docs(event, context):
-    github_token = get_secret("github_token")
+    github_webhook_token = get_secret("github_webhook_token")
     headers = event["headers"]
 
     # Make sure request came from github
@@ -89,7 +96,7 @@ def build_docs(event, context):
         raise Exception("Bad Request")
 
     # Make sure request has valid token
-    signature_response = validate_signature(headers, event["body"], github_token)
+    signature_response = validate_signature(headers, event["body"], github_webhook_token)
     if signature_response is not None:
         response = {
             "statusCode": 500,
@@ -102,9 +109,9 @@ def build_docs(event, context):
         event["body"] = base64.b64decode(event['body'])
 
     json_payload = json.loads(event["body"])
-
     github_repo = json_payload["repository"]['html_url']
     pr_number = json_payload["number"]
+    action = json_payload["action"]
 
     if not docs_files_changed(pr_number):
         response_body = "no docs changes"
